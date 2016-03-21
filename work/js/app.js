cf187fbae273fc5b4d000a469655ebc005377cd9
goodwid <goodwid@gmail.com>
2016-03-21 14:48:57 -0700

    Prepped app.js and HTML for problem 3

--- work/js/app.js
+++ work/js/app.js
@@ -12,3 +12,11 @@ var p2arg1 = 4;
 var p2arg2 = 7;
 problemTwoIn.textContent  = 'Calling multiply() with an arguments of ' + p2arg1 + ' and ' + p2arg2 +'.';
 problemTwoOut.textContent = 'multiply() returned ' + multiply(p2arg1,p2arg2);
+
+var problemThreeIn  = document.getElementById('problemThreeIn');
+var problemThreeOut = document.getElementById('problemThreeOut');
+var p3arg1 = 3;
+var p3arg2 = 4;
+var p3arg3 = 8;
+problemThreeIn.textContent  = 'Calling sumAndMultiply() with an arguments of ' + p3arg1 + ', ' + p3arg2 + ', and ' + p3arg3 + '.';
+problemThreeOut.textContent = 'sumAndMultiply() returned ' + sumAndMultiply(p3arg1,p3arg2,p3arg3);
