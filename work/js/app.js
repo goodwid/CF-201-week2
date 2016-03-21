a055a523d1ca597e8740f6dc1f4f72efe44f21e9
goodwid <goodwid@gmail.com>
2016-03-21 13:35:33 -0700

    added tests for problem 1

--- work/js/app.js
+++ work/js/app.js
@@ -1,6 +1,1 @@
-var pAddFiveIn  = document.getElementById('pAddFiveIn');
-var pAddFiveOut = document.getElementById('pAddFiveOut');
 
-var arg = 10;
-pAddFiveIn.textContent  = 'Calling addFive() with an argument of ' + arg;
-pAddFiveOut.textContent = 'addFive() returned ' + addFive(arg);
--- work/test/test.js
+++ work/test/test.js
@@ -1,9 +1,18 @@
 var assert = require('assert');
-var compute = require('../js/compute.js');
+var app = require('../js/app.js');
 
-describe('Goodwin ', function() {
+describe('Goodwin function tests:', function() {
   // it('should check first question', function() {     assert.deepEqual(compute.getQuestion(0), 'first');   });
 
+    it ('should return 8 when adding 3 and 5', function() {
+        assert.deepEqual(app.sum(3,5),8);
+    });
+    it ('should return 7 when adding -2 and 9', function() {
+        assert.deepEqual(app.sum(-2,9),7);
+    });
+    it ('should return "fourfive" when adding "four" and "five"', function() {
+        assert.deepEqual(app.sum("four","five"),"fourfive");
+    });
 
 
 
