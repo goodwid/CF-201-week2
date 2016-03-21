015f130dbd91cddd48d877f3d81cf9eea6744288
goodwid <goodwid@gmail.com>
2016-03-21 14:01:24 -0700

    Switched stuff around to restore the roles of app.js, and compute.js

--- work/js/app.js
+++ work/js/app.js
@@ -1,10 +1,12 @@
 
 
-function sum(a,b) {
-    c=a+b;
-    console.log('The sum of '+ a +' and ' + b +' is ' + c + '.');
-    return c;
 
-}
 
-exports.sum = sum;
+
+
+var problemOneIn  = document.getElementById('problemOneIn');
+var problemOneOut = document.getElementById('problemOneOut');
+var arg1 = 10;
+var arg2 = 13;
+problemOneIn.textContent  = 'Calling sum() with an arguments of ' + arg1 + ' ' + arg2 +'.';
+problemOneOut.textContent = 'sum() returned ' + sum(arg1,arg2);
--- work/test/test.js
+++ work/test/test.js
@@ -1,17 +1,17 @@
 var assert = require('assert');
-var app = require('../js/app.js');
+var compute = require('../js/compute.js');
 
 describe('Goodwin problem 1:', function() {
   // it('should check first question', function() {     assert.deepEqual(compute.getQuestion(0), 'first');   });
 
     it ('should return 8 when adding 3 and 5', function() {
-        assert.deepEqual(app.sum(3,5),8);
+        assert.deepEqual(compute.sum(3,5),8);
     });
     it ('should return 7 when adding -2 and 9', function() {
-        assert.deepEqual(app.sum(-2,9),7);
+        assert.deepEqual(compute.sum(-2,9),7);
     });
     it ('should return "fourfive" when adding "four" and "five"', function() {
-        assert.deepEqual(app.sum("four","five"),"fourfive");
+        assert.deepEqual(compute.sum("four","five"),"fourfive");
     });
 
 });
