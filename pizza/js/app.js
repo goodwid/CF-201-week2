b9c942a793c1e1c95e03c6b7c59f775503e90752
goodwid <goodwid@gmail.com>
2016-03-22 12:12:24 -0700

    createPizzaData created.

--- pizza/js/app.js
+++ pizza/js/app.js
@@ -1,21 +1,15 @@
 var exports = {};
-document.addEventListener('DOMContentLoaded', function() {
+//document.addEventListener('DOMContentLoaded', function() {
+var compute = require('../js/compute.js');
 
-var restaurants = {
+var restaurants = [{
     location: "Beaverton",
-    hoursOpen: [800,200],
+    hoursOpen: [8,26], // 26 = 2am the following morning
     dayOpen: [0,2,3,4,5,6,7],
     manager: "Bob Jones",
     managerImage: "",
     phoneNumber: "555-34-PIZZA",
-    pizzaCount: {
-        block1: [0,4],
-        block2: [0,7],
-        block3: [2,15,
-        block4: [15,35],
-        block5: [12,31],
-        block6: [5,20]
-    },
+    pizzaCount: [[0,4],[0,7],[2,15],[15,35],[12,31],[5,20]],
     deliveryCount: {
         block1: [0,4],
         block2: [0,4],
@@ -23,12 +17,33 @@ var restaurants = {
         block4: [3,8],
         block5: [5,12],
         block6: [6,11]
-    },
-    driversNeeded: function (hourlyDeliveries) {
-        return hourlyDeliveries % 3;
     }
+}];
 
-};
+// var Shop = function () {
+//     driversNeeded: function () {
+//         return hourlyDeliveries % 3;
+//     }
+// };
 
 
-});
+function createPizzaData (open,close,pizzaCount) {
+        var hour,i;
+        var blockcount=0;
+        var results = [];
+        var low,high;
+        for (hour = open ; hour < close ; hour++) {
+            low = pizzaCount[(blockcount-blockcount%3)/3][0];
+            high = pizzaCount[(blockcount-blockcount%3)/3][1];
+            results.push ([hour,compute.randomRange(low,high)]);
+            blockcount++;
+        } // for i
+        return results;
+    }  // for function
+
+
+console.log (createPizzaData(   restaurants[0].hoursOpen[0],
+                                restaurants[0].hoursOpen[1],
+                                restaurants[0].pizzaCount));
+
+//});  // addEventListener
