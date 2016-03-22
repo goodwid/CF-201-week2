a40c68c1cd648e32e93c0aaf86f7c1ed1de44b7e
goodwid <goodwid@gmail.com>
2016-03-22 11:05:18 -0700

    Working model of object for a store in progress.

--- pizza/js/app.js
+++ pizza/js/app.js
@@ -3,7 +3,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
 var restaurants = {
     location: "Beaverton",
+    hoursOpen: [800,200],
+    dayOpen: [0,2,3,4,5,6,7],
+    manager: "Bob Jones",
+    managerImage: "",
+    phoneNumber: "555-34-PIZZA",
+    pizzaCount: {
+        block1: [0,4],
+        block2: [0,7],
+        block3: [2,15,
+        block4: [15,35],
+        block5: [12,31],
+        block6: [5,20]
+    },
+    deliveryCount: {
+        block1: [0,4],
+        block2: [0,4],
+        block3: [1,4],
+        block4: [3,8],
+        block5: [5,12],
+        block6: [6,11]
+    },
+    driversNeeded: function (hourlyDeliveries) {
+        return hourlyDeliveries % 3;
+    }
 
 };
+
 
 });
--- pizza/js/compute.js
+++ pizza/js/compute.js
@@ -2,9 +2,3 @@ function randomRange (low,high) {
  return Math.floor(Math.random()*(high-low))+low;
 }
 exports.randomRange = randomRange;
-
-for (var i=0;i<1000;i++) {
-    n = randomRange(20,29);
-    if (n < 20) {console.log ('low' + i);}
-    if (n > 29) {console.log ('high' + i);}
-}
